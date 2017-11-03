3b7930afeb33fdcb2a42214b4055d7c2c501774f
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2017-11-03 18:58:54 -0300

    commit inicial

---
+++ AreaCirculo/AreaCirculo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AreaCirculo
+{
+    public partial class MainPage : ContentPage
+    {
+        public double? Radius { get; set; }
+        public double? Area { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        private void ButtonCalculate_Clicked(object sender, EventArgs e)
+        {
+            if (Radius.HasValue)
+            {
+                Area = Radius.Value * 2 * Math.PI;
+
+                OnPropertyChanged(nameof(Area));
+            }
+        }
+    }
+}
